81727f48b03c96052033a23826db022c515606a9
RAKESH40-lgtm <ram19042k@protonmail.com>
2026-01-31 15:36:43 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-3xl text-blue bg-blue-800">
+        Start journey
+      </p>
+    </>
+  )
+}
+
+export default App
